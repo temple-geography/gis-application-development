14dfb0c563ae8fe7cb884b0c0b1820b0d0f3ebdf
Tate Kamish <tatekamish@gmail.com>
2022-02-08 17:48:16 -0500

    time and space workshop

--- classes_and_oop/shapes_naive.py
+++ classes_and_oop/shapes_naive.py
@@ -16,12 +16,12 @@ class Polygon:
     
     def __init__(self, coords):
         self.coords = coords
-        x = [c[0] for c in coords]
-        y = [c[1] for c in coords]
-        s = 0
-        perim = 0
+        x = [c[0] for c in coords]   #list comprehension
+        y = [c[1] for c in coords]   # list comprehension
+        s = 0        #placeholder before loop
+        perim = 0    #placeholder before loop
         for i in range(len(coords) - 1):
-            s += x[i]*y[i+1] - x[i+1]*y[i]
+            s += x[i]*y[i+1] - x[i+1]*y[i] #adding value to s and setting it equal to that
             perim += sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
        
         self.area = s/2
@@ -34,7 +34,7 @@ class Polygon:
     
     def __init__(self, coords):
         self.coords = coords
-        x = [c[0] for c in coords]
+        x = [c[0] for c in coords] 
         y = [c[1] for c in coords]
         s = 0
         perim = 0
@@ -42,12 +42,14 @@ class Polygon:
             s += x[i]*y[i+1] - x[i+1]*y[i]
             perim += sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
        
-        self._area = s/2
-        self._perimeter = perim
+        self._area = s/2           #private attribute 
+        self._perimeter = perim    #private attribute 
 
-    @property
+    @property    #decorator 
     def area(self):
-        return self._area
+        return self._area  #returns value of private attribute 
+    
+#this stops the user from assigning their own value to the property
 
     @property
     def perimeter(self):
@@ -57,3 +59,40 @@ if __name__ == "__main__":
     
     poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
 
+#%%
+class Polygon:
+    
+    def __init__(self, coords):
+        self.coords = coords
+
+    @property    
+    def area(self):
+        return self._area  
+
+    @property
+    def perimeter(self):
+        return self._perimeter
+    
+    @property
+    def coords(self):
+        return self._coords
+
+    
+    @coords.setter
+    def coords(self,coords):
+        self._coords = coords
+        x = [c[0] for c in coords] 
+        y = [c[1] for c in coords]
+        s = 0
+        perim = 0
+        for i in range(len(coords) - 1):
+            s += x[i]*y[i+1] - x[i+1]*y[i]
+            perim += sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
+       
+        self._area = s/2          
+        self._perimeter = perim
+       
+           
+if __name__ == "__main__":
+    
+    poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
