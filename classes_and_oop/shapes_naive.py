ea92bc5cb7847bbcb74da6b0c64063fa18c6a110
Casey Smith <caseysmith.pgh@gmail.com>
2022-02-08 17:48:54 -0500

    hi

--- classes_and_oop/shapes_naive.py
+++ classes_and_oop/shapes_naive.py
@@ -56,4 +56,44 @@ class Polygon:
 if __name__ == "__main__":
     
     poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
+    
+    
+#%%
+# Fifth attempt with private attributes
+
+class Polygon:
+    
+    def __init__(self, coords):
+        self.coords = coords
+
+    @property
+    def area(self):
+        return self._area
+
+    @property
+    def perimeter(self):
+        return self._perimeter
+    
+    @property
+    def coords(self):
+        return self._coords
+    
+    @coords.setter
+    def coords(self, coords):
+        self._coords = coords
+        x = [c[0] for c in coords]
+        y = [c[1] for c in coords]
+        s = 0
+        perim = 0
+        for i in range(len(coords) - 1):
+            s += x[i]*y[i+1] - x[i+1]*y[i]
+            perim += sqrt((x[i+1] - x[i])**2 + (y[i+1] - y[i])**2)
+            
+        self._area = s/2
+        self._perimeter = perim
+           
+if __name__ == "__main__":
+    
+    poly = Polygon([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
+    
 
